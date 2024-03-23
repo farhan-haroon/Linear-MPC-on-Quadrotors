2194d41c52d1fe5f2be8c129e37ed9133ae3a6b9
Mohd Farhan Haroon <109333218+farhan-haroon@users.noreply.github.com>
2024-03-24 01:48:26 +0530

    Delete +bus_conv_fcns/+ros directory

--- +bus_conv_fcns/+ros/+busToMsg/+std_msgs/Float64.m
+++
@@ -1,5 +0,0 @@
-function rosmsgOut = Float64(slBusIn, rosmsgOut)
-%#codegen
-%   Copyright 2021 The MathWorks, Inc.
-    rosmsgOut.Data = double(slBusIn.Data);
-end
--- +bus_conv_fcns/+ros/+busToMsg/+std_msgs/Int32.m
+++
@@ -1,5 +0,0 @@
-function rosmsgOut = Int32(slBusIn, rosmsgOut)
-%#codegen
-%   Copyright 2021 The MathWorks, Inc.
-    rosmsgOut.Data = int32(slBusIn.Data);
-end
